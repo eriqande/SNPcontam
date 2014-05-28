b4db002076aaa6bbcced33a0ce2503a122f9c8f3
evenable <Elena_venable@brown.edu>
2014-05-28 15:57:04 -0700

    Updated likelihood function that outputs geneotype probabilities as well as likelihood of genotypes.

--- R/likelihood.R
+++ R/likelihood.R
@@ -2,13 +2,17 @@
 likelihood <- function(gt,af) {
   #calculate probablitiy for noncontaminated
   g0 <- af^2; g1 <- 2*af*(1-af); g2 <- (1-af)^2
+  gprob <- rbind(g0,g1,g2)
   #calculate probability for contaminated
-  gc0 <- af^4; gc1 <- 1 - (af^4 + (1-af)^4); gc2 <- (1-af)^4 
+  gc0 <- af^4; gc1 <- 1 - (af^4 + (1-af)^4); gc2 <- (1-af)^4
+  gprobc <- rbind(gc0, gc1, gc2)
   #make vector of likelihood for each non contaminated gene
-  a <- gt==0; b <- gt==1; c <- gt==2
-  g <- g0*a + g1*b + g2*c
+  if(gt>0){
+    a <- gt==0; b <- gt==1; c <- gt==2
+    g <- g0*a + g1*b + g2*c
   #make vector of likelihood for each contaminated gene
-  ac <- gt==0; bc <- gt==1; cc <- gt==2
-  gc <- gc0*ac + gc1*bc + gc2*cc
-  list(contam = gc, clean = g)
+    ac <- gt==0; bc <- gt==1; cc <- gt==2
+    gc <- gc0*ac + gc1*bc + gc2*cc
+  }  
+  list(contam = gc, clean = g, contam_prob = gprobc, clean_prob = gprob)
 }
