fbe1af6c387666efd5cb897e05b7806ee6c41cca
Eric C. Anderson <eric.anderson@noaa.gov>
2014-07-02 11:33:31 -0700

    Alter MCMC_ztable to use the table function.

--- not-package/simulations/simulation_functions.R
+++ not-package/simulations/simulation_functions.R
@@ -254,20 +254,22 @@ allele_table <- function(types, data, Lvals, rhovals){
 }
 
 ## Creates a matrix with information for the z value table
-MCMC_ztable <- function(z_df,types,rhovals,Lvals){
-  get_zstats <- function(y,z){
-    sub <- z_df[z_df$contam_prob==y & z_df$loci_number==z,]
-    t1 <- length(sub$z[sub$z_id==1])
-    t0 <- length(sub$z[sub$z >= .9])
-    frac_1 <- length(sub$z[sub$z_id==1 & sub$z >= .9])/t1
-    frac_0 <- length(sub$z[sub$z_id==0 & sub$z >= .9])/t0
-    list(frac_1,frac_0)
-  }
-  tmp <- lapply(types,function(x) {get_zstats(y = x$rho, z = x$numL)})
-  values <- lapply(tmp, function(x) {if (is.na(x[[1]])) {x[[1]] = NA}; if (is.na(x[[2]])){x[[2]] = 0}; return(x)})
-  mat <- round(matrix(unlist(values), nrow=length(rhovals)),4)
-  rownames(mat) <- paste0(rhovals)
-  colnames(mat) <- paste0(rep(Lvals,each=2)," Loci")
-  return(mat)
+## z is the z_output data frame from the simulation.  PPlim
+## is the posterior prob cutoff for which you call something
+## contaminated.  We will use 0.5.
+MCMC_ztable <- function(z, PPlim = 0.5) {
+  # NP <== "No Posterior"  (We do not include posterior >0.9 as a factor)
+  NP <- table(list(rho=z$contam_prob, L=z$loci_number, Z=z$z_id) )
+  
+  # WP <== "with posterior >0.9" included as a factor
+  WP <- table(list(rho=z$contam_prob, L=z$loci_number, Z=z$z_id, GreaterThanPPlim=z$z>PPlim) )
+  
+  # uncontams with post prob > PPlim
+  FalsePos <- WP[,,"0","TRUE"] / NP[,,"0"]
+  
+  # true contams with post prob > PPlim
+  TruePos <- WP[,,"1","TRUE"] / NP[,,"1"]
+  
+  list(FP = FalsePos, TP = TruePos)
 }
 
