1a675c6293e45fd4666e7ab44f63e8681944c2e1
evenable <Elena_venable@brown.edu>
2014-07-07 12:32:29 -0700

    Added some code to sim_mixture to simulate contaminated samples.

--- not-package/notes/sim_mixture.R
+++ not-package/notes/sim_mixture.R
@@ -1,7 +1,9 @@
 library(fullsniplings)
 
 # Setting up the data from the baseline for the function
-N = 50
+N = 100
+p = .05
+N_c = N*p
 swfs <- swfsc_chinook_baseline
 swfs2 <- data.frame(lapply(swfs, as.character), stringsAsFactors=FALSE)
 Pop <- swfs$Pop
@@ -12,12 +14,27 @@ for (i in 2:8031){
     k = k+1}
   swfs2$Pop[i] = k
 }
-a <- sample(1:8031,N)
+a <- sample(1:8031,(N - N_c))
 genos <- swfs2[a,-(1:4)]
+
+# Contaminated Genotypes
+contam1 <- sample(1:8031,N_c)
+contam2 <- sample(1:8031,N_c)
+geno1 <- get_snp_genos(swfs2[contam1,-(1:4)])$mat
+geno2 <- get_snp_genos(swfs2[contam2,-(1:4)])$mat
+geno_c <- geno1 + geno2
+geno_c[geno_c == 4] = 2
+geno_c[geno_c == 3] = 1
+geno_c[geno1 ==1 & geno2 == 1] = 1
+geno_c[(geno1 == 0 & geno2 == 2)|(geno1 == 2 & geno2 == 0)] = 1
+geno_c[is.na(geno1) & is.na(geno2) == FALSE] = geno2[is.na(geno1) & is.na(geno2) == FALSE]
+geno_c[is.na(geno2) & is.na(geno1) == FALSE] = geno1[is.na(geno2) & is.na(geno1) == FALSE]
+
 pop_id <- swfs2$Pop[a]
+contam_id <- rbind(swfs2$Pop[contam1],swfs2$Pop[contam2])
 
 snp_genos <- get_snp_genos(genos)
-data <- snp_genos$mat
+data <- cbind(snp_genos$mat,geno_c)
 
 boing <- lapply(1:69, function(x) {subset(swfs2,swfs2$Pop == x)[,-(1:4)]})
 tmp <- lapply(boing, function(x) {snp_genos = get_snp_genos(x)})
@@ -44,19 +61,20 @@ z_pm <- colMeans(test$z)
 
 # Create Table for Population Data
 true_pop <- pop_id
-clean_pops <- lapply(1:N, function(x) {test$pops[,x][2*which(test$z[,x] == 0) - 1]})
+clean_pops <- lapply(1:(N-N_c), function(x) {test$pops[,x][2*which(test$z[,x] == 0) - 1]})
 tmp_pops <- lapply(clean_pops, function(x) {factor(x,level = pops)}) 
 freq <- lapply(tmp_pops, function(x) {as.numeric(table(x))})
 MCMC_pop <- sapply(freq, function(x) {pops[which(x == max(x))]}) 
-Rep_us <- sapply(1:N, function(x) {swfs2$RepUnit[swfs2$Pop==MCMC_pop[x]][1] == swfs2$RepUnit[swfs2$Pop==true_pop[x]][1]})
+Rep_us <- sapply(1:(N-N_c), function(x) {swfs2$RepUnit[swfs2$Pop==MCMC_pop[x]][1] == swfs2$RepUnit[swfs2$Pop==true_pop[x]][1]})
 pop_df <- data.frame(True_Population = true_pop, MCMC_Population = MCMC_pop, Same_RepUnit = Rep_us)
 
 # Proportion of Individuals Placed in Correct RepUnit
-correct_p = sum(Rep_us)/N
+correct_p = sum(Rep_us)/(N-N_c)
 
 # Assess Mixture Data
 count_pop <- as.numeric(table(as.numeric(swfs2$Pop)))
 freqs_pop <- count_pop/sum(count_pop)
 mixing <- colMeans(test$mixing)
 mix_df <- data.frame(True_mix = freqs_pop, MCMC_mix = mixing, Difference = freqs_pop - mixing)
+
 
