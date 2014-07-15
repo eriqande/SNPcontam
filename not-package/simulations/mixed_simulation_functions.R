f1730e580fde3dc3f83d00c8fe870a7ed23ac1ee
evenable <Elena_venable@brown.edu>
2014-07-15 16:28:57 -0700

    Finished code for the simulation of the mixture MCMC.
    Finished the code for all the functions needed to run the MCMC.  No sure if it is debugged yet because I am still running a little to test run to check for bugs. Still need to change a few things with the input and output of the function.

--- not-package/simulations/mixed_simulation_functions.R
+++ not-package/simulations/mixed_simulation_functions.R
@@ -87,6 +87,7 @@ make_mixture <- function(baseline, N, p, clean_list = NULL, contam_list = NULL){
 
 # Get clean_list and contam list for simulation
 mixed_lists <- function(baseline, props, N ,p , less.NA = NULL){
+  N_c = N*p
   if(length(less.NA) > 0){
       num_na <- rowSums(is.na(baseline))
       newb <- baseline[-which(num_na > less.NA),]
@@ -102,9 +103,9 @@ mixed_lists <- function(baseline, props, N ,p , less.NA = NULL){
 
 # Simulation Function
 mixed_MCMC_sims <- function(baseline, N, p, fish_pops,inters,contamination = TRUE, less.NA = NULL, n){
-  
+  P = length(levels(baseline$Pop))
   # function for simplifying MCMC data
-  mixed_MCMC_with_means <- function(baseline, N, p, contamination = TRUE, inters, props, less.NA = NULL, alpha=.5, beta=.5){
+  mixed_MCMC_with_means <- function(baseline, N, p, contamination = TRUE, inters, props, less.NA = NULL, alpha=.5, beta=.5, burnin = 100){
     N_c = N*p
     mixture_lists <- mixed_lists(baseline, props, N, p, less.NA = less.NA)
     b <- make_mixture(baseline,N,p,clean_list = mixture_lists$clean_list, contam_list = mixture_lists$contam_list)
@@ -112,56 +113,104 @@ mixed_MCMC_sims <- function(baseline, N, p, fish_pops,inters,contamination = TRU
     contam_mat <- Pcontam(snp_zeroes = MCMC_mats$zero, snp_ones = MCMC_mats$ones, genos = MCMC_mats$mixmat, lambda = .5)
     clean_mat <- P_likelihood(snp_zeroes = MCMC_mats$zero, snp_ones = MCMC_mats$ones, genos = MCMC_mats$mixmat, lambda = .5)
     MCMC_data <- mixed_MCMC(MCMC_mats$mixmat, contam_mat, clean_mat, inters = inters, contamination = TRUE)
-    z_pm <- colMeans(MCMC_data$z)
-    z_id <- c(rep(0,N),rep(1,N*p))
-    rho_pm <- mean(MCMC_data$prob_contam)
+    z_pm <- colMeans(MCMC_data$z[-(1:burnin),])
+    z_id <- c(rep(0,(N-N_c)),rep(1,N*p))
+    rho_pm <- mean(MCMC_data$prob_contam[-(1:burnin)])
     P = nrow(clean_mat)
+    
+    # Mixing proportions (pii)
+    pii = colMeans(MCMC_data$mixing[-(1:burnin),])
+    
     # population assignment for clean populations
     if (contamination){
-      clean_pops <- lapply(1:(N-N_c), function(x) {MCMC_data$pops[,x][2*which(MCMC_data$z[,x] == 0) - 1]})
+      clean_pops <- lapply(1:(N-N_c), function(x) {MCMC_data$pops[-(1:(2*burnin)),x][2*which(MCMC_data$z[-(1:(2*burnin)),x] == 0) - 1]})
       tmp_pops <- lapply(clean_pops, function(x) {factor(x,level = 1:P)}) 
       freq <- lapply(tmp_pops, function(x) {as.numeric(table(x))})
       pop_means <- do.call(what = rbind, args = lapply(freq, function(x) x))/inters
       colnames(pop_means) <- levels(b$bline$RepPop)
       if (p == 0) {rownames(pop_means) <- rownames(b$mixture)}else {rownames(pop_means) <- rownames(b$mixture)[1:(N-N_c)]}
+      #contaminated population data
+      contam_pops <- lapply((N-N_c + 1):N, function(x) {if (z_pm[x] == 1){MCMC_data$pop[-(1:(2*burnin)),x]} else{MCMC_data$pops[-(1:(2*burnin)),x][-c((2*which(MCMC_data$z[-(1:(2*burnin)),x] == 0) - 1),(2*which(MCMC_data$z[-(1:(2*burnin)),x] == 0)))]}})
     }else {
-      tmp_pops <- lapply(1:N, function(x) {factor(MCMC_data$pop[,x],level = 1:P)}) 
+      tmp_pops <- lapply(1:N, function(x) {factor(MCMC_data$pop[-(1:burnin),x],level = 1:P)}) 
       freq <- lapply(tmp_pops, function(x) {as.numeric(table(x))})
       pop_means <- do.call(what = rbind, args = lapply(freq, function(x) x))/inters
       colnames(pop_means) <- levels(b$bline$RepPop)
       rownames(pop_means) <- rownames(b$mixture)
     }
     
-    #contaminated population data
-    contam_pops <- lapply((N-N_c + 1):N, function(x) {if (z_pm[x] == 1){MCMC_data$pop[,x]} else{MCMC_data$pops[,x][-c((2*which(MCMC_data$z[,x] == 0) - 1),(2*which(MCMC_data$z[,x] == 0)))]}})
     
-    list(z_pm = z_pm , z_id = z_id, rho_pm = rho_pm, clean_means = pop_means, contam_pops = contam_pops, MCMC = MCMC_data, mixture_ids = rownames(b$mixture))  
+    
+    list(z_pm = z_pm , z_id = z_id, rho_pm = rho_pm, clean_means = pop_means, contam_pops = contam_pops, MCMC = MCMC_data, mixing_props = pii, mixture_ids = rownames(b$mixture))  
   }
   
   # run MCMC on all simulation mixtures
-  types <- lapply(1:nrow(fish_pops), function(x) list(fish_pops[x,]))
-  names(types) <- rownames(fish_pops)
+  types <- lapply(1:nrow(fish_pops), function(x) list(pop = fish_pops[x,], name = rownames(fish_pops)[x]))
   MCMC <- lapply(types, function(x) {
     lapply(1:n, function(y) {
-      list(params = names(x), output = mixed_MCMC_with_means(baseline = baseline, N = N, p = p, contamination = contamination, inters = inters, props = unlist(x), less.NA = less.NA))
+      list(params = list(name = x$name, proportions = x$pop), output = mixed_MCMC_with_means(baseline = baseline, N = N, p = p, contamination = contamination, inters = inters, props = unlist(x$pop), less.NA = less.NA))
     })
   }) #mc.cores=length(types))
   
   # MAkes the z output data frame
   slurp_mcmc_z_output2 <- function(y) {
-    data.frame(z = y$output$z_pm, z_id = y$output$z_id, fishery_props = y$params)
+    data.frame(z = y$output$z_pm, z_id = y$output$z_id, fishery = y$params$name, ID = y$output$mixture_ids)
   }
-  ztmp1 <- mclapply(1:length(MCMC), function(rep) {lapply(MCMC[[rep]], function(x) {df <- slurp_mcmc_z_output(x); df$rep_num=rep; df }) }) 
+  ztmp1 <- mclapply(1:length(MCMC), function(rep) {lapply(MCMC[[rep]], function(x) {df <- slurp_mcmc_z_output2(x); df$rep_num=rep; df })}) 
   ztmp2 <- unlist(ztmp1, recursive = FALSE)
   z_df <- do.call(what = rbind, args = ztmp2)
   
   # Makes the rho output data frame
   slurp_mcmc_rho_output2 <- function(y){
-    data.frame(rho_pm = y$output$rho_pm, cfishery_props = y$params)
+    data.frame(rho_pm = y$output$rho_pm, fishery = y$params$name, ID = y$output$mixture_ids)
   }
-  rtmp1 <- mclapply(1:length(MCMC), function(rep) {lapply(MCMC[[rep]], function(x) {df <- slurp_mcmc_rho_output(x); df$rep_num=rep; df})})
+  rtmp1 <- mclapply(1:length(MCMC), function(rep) {lapply(MCMC[[rep]], function(x) {df <- slurp_mcmc_rho_output2(x); df$rep_num=rep; df})})
   rtmp2 <- unlist(rtmp1, recursive = FALSE)
   rho_df <- do.call(what = rbind, args = rtmp2)
   
-  list(z_df = z_df, rho_df = rho_df)
+  # MAkes the mixing output data frame
+  slurp_mcmc_pii_output <- function(y){
+    colnames(y$params$proportions) <- NULL
+    rownames(y$params$proportions) <- NULL
+    data.frame(populations = colnames(y$output$clean_means),true_mixing = t(y$params$proportions), mixing_pm = y$output$mixing_props, fishery = y$params$name)
+  }
+  mtmp1 <- mclapply(1:length(MCMC), function(rep) {lapply(MCMC[[rep]], function(x) {df <- slurp_mcmc_pii_output(x); df$rep_num=rep; df})})
+  mtmp2 <- unlist(mtmp1, recursive = FALSE)
+  pii_df <- do.call(what = rbind, args = mtmp2)
+  
+  # Makes data frame for clean population means
+  slurp_mcmc_u_output <- function(y){
+    data.frame(fishery = y$params$name, u = y$output$clean_means)
+  }
+  utmp1 <- mclapply(1:length(MCMC), function(rep) {lapply(MCMC[[rep]], function(x) {df <- slurp_mcmc_u_output(x); df$rep_num=rep; df})})
+  utmp2 <- unlist(utmp1, recursive = FALSE)
+  u_df <- do.call(what = rbind, args = utmp2)
+  
+  #Contam Data
+  #Find maximum pair
+  opts <- combinations(P,2,1:P, repeats.allowed = TRUE)
+  get_contam_combos <- function(ind,opts){
+    t_opts <- nrow(opts)
+    mat_ind <- lapply(ind$output$contam_pops, function(x) matrix(x,ncol=2, byrow = TRUE))
+    counts <- lapply(mat_ind, function(y) {sapply(1:t_opts, function(x) {sum(y[,1] == opts[x,1] & y[,2] == opts[x,2] | y[,1] == opts[x,2] & y[,2] == opts[x,1])})})
+    max_pair <- sapply(counts, function(x) {a <- which(max(x) == x); if(length(a) > 1){a2 <- a[[1]]} 
+                                            else{a2 <- a}; opts[a2,]})
+    counts2 <- lapply(counts, function(x) {a <- which(max(x) == x); if(length(a) > 1){a2 <- a[[1]]} 
+                                            else{a2 <- a}; x[-a2]})
+    max_pair2 <- sapply(counts2, function(x) {a <- which(max(x) == x); if(length(a) > 1){a2 <- a[[1]]} 
+                                            else{a2 <- a}; opts[a2,]})
+    
+    populations = colnames(ind$output$clean_means)
+    bb <- data.frame(ID = ind$output$mixture_ids[(N-N_c + 1):N], 
+               max_pair = matrix(populations[t(max_pair)],ncol=2), max_pair2 = 
+                 matrix(populations[t(max_pair2)], ncol=2))
+  }
+  pair_tmp <- mclapply(1:length(MCMC), function(rep) {lapply(MCMC[[rep]], function(x) {get_contam_combos(x,opts)})})
+  pair_tmp2 <- unlist(pair_tmp, recursive = FALSE)
+  contam_u_df <- do.call(what = rbind, args = pair_tmp2)
+  
+  #Just get all of the contam data
+  contam_data <- mclapply(1:length(MCMC), function(rep) {lapply(MCMC[[rep]], function(x) {x$output$contam_pops})})
+  
+  list(z_df = z_df, rho_df = rho_df, pii_df, u_df = u_df , contam_u_df = contam_u_df, contam_data = contam_data)
 }
